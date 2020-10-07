e3ab209302b9fba895a95277cf065075506fa573
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-10-07 14:59:37 +0000

    Fix setup.py to include template

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setup(
     name="pvserver-webproxy",
     packages=find_packages(),
     version='0.1.0',
-    zip_safe=False
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=['tornado'],
 )
